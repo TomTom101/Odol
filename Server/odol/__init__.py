0f753912190002eaa5bc237e6fcdcaf7b9590ca1
Thomas Brandl <thobra@gmail.com>
2013-02-26 08:58:53 +0100

    Create log in new log path

--- Server/odol/__init__.py
+++ Server/odol/__init__.py
@@ -8,22 +8,10 @@ __license__ = "GPLv3"
 __versioninfo__ = (0, 0, 2)
 __version__ = '.'.join(map(str, __versioninfo__))
 
-
 class DataFilter(logging.Filter):
 	""" ignore DATA level type "errors" of levelno 100 """
 	def filter(self, record):
 		return record.levelno <= logging.CRITICAL
-	
-logging.addLevelName(100, 'DATA')
-logger = logging.getLogger(__name__)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh = logging.FileHandler(__name__ + '.log')
-fh.addFilter(DataFilter())
-fh.setLevel(logging.INFO)	
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-logger.setLevel(logging.INFO)
 
 config = ConfigParser.ConfigParser()
 config_file = __name__ + '.cfg'
@@ -33,3 +21,16 @@ config_path = os.path.join(os.path.expanduser("~/odol"), config_file)
 config.readfp(open(def_config_path))
 config.set('data', 'home_path', os.path.expanduser("~"))
 config.read(config_path)
+	
+logging.addLevelName(100, 'DATA')
+logger = logging.getLogger(__name__)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh = logging.FileHandler(os.path.join(config.get('data', 'log_path'), __name__ + '.log'))
+print os.path.join(config.get('data', 'data_path'), __name__ + '.log')
+fh.addFilter(DataFilter())
+fh.setLevel(logging.INFO)	
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.setLevel(logging.INFO)
+
