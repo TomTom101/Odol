a8b794431995e719b50e721d6c5e7abf361279a8
Thomas Brandl <thobra@gmail.com>
2013-12-15 18:52:33 +0100

    * copy the latest distribution file

--- Server/scripts/update_raspberry.sh
+++ Server/scripts/update_raspberry.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-python setup.py sdist && scp dist/odol-0.1.tar.gz  pi@192.168.0.18:/home/pi
+latest=`ls -t dist/odol* | head -1`
+python setup.py sdist && scp $latest pi@192.168.0.18:/home/pi
