ce9996c6a6c3fb02c7c98450cee991860910757b
Thomas Brandl <thobra@gmail.com>
2013-12-15 18:52:48 +0100

    * update to 0.22

--- Server/setup.py
+++ Server/setup.py
@@ -7,10 +7,10 @@ config = {
     'description': 'Ambient Light Sensor connected to Arduino',
     'author': 'Thomas Brandl',
     'author_email': 'thobra@gmail.com',
-    'version': '0.1',
+    'version': '0.22',
     'install_requires': ['nose', 'python-eeml', 'pyserial', 'facebook-sdk', 'numpy'],
     'packages': ['odol', 'tests'],
-    'scripts': ['scripts/service.py', 'scripts/service.sh', 'scripts/odol_draw.py'],
+    'scripts': ['scripts/odol-service.py', 'scripts/odol-draw.py'],
     'name': 'odol'
 }
 
